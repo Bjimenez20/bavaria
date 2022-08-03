1c4782dddd62037b658399ec01c45067bcf2e96c
Brayan20 <aprenprogramador@app-peoplemarketing.com>
2022-08-03 08:54:01 -0500

    Update(v1.4)
    Implementacion de logos

--- presentacion/layouts/sidebar.php
+++ presentacion/layouts/sidebar.php
@@ -10,7 +10,7 @@
                 <a href="http://www.creative-tim.com" class="simple-text logo-normal">
                     <div class="row-reverse">
                         <div class="col">
-                            <img src="./../presentacion/imagenes/1.png" alt="" id="logo_max">
+                            <img src="./../presentacion/imagenes/6.png" alt="" id="logo_max">
                         </div>
                         <div class="col d-flex justify-content-center">
                             <label for="" class="text-white ">IPSEN</label>
