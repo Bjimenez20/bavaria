4e997c62b137d132bd47c6c280e6206d0edf7f16
Brayan Mauricio Jimenez Salamanca <bjimenez@app-peoplemarketing.com>
2023-01-12 15:35:21 -0500

    Update form_bi.php

--- presentacion/form_bi.php
+++ presentacion/form_bi.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-<iframe title="Como vamos - INICIO" width="100%" height="695.5" src="@" frameborder="0" allowFullScreen="true"></iframe>
+<!-- <iframe title="Como vamos - INICIO" width="100%" height="695.5" src="@" frameborder="0" allowFullScreen="true"></iframe> -->
+<iframe title="IPSEN" width="100%" height="695.5" src="https://app.powerbi.com/view?r=eyJrIjoiZDZmZjlhZmEtMzBjNC00MGE3LTgyNDUtMTg5YTI2M2IwZTVlIiwidCI6ImU5MDEyNjRhLTA5ZWQtNGUyMy1iMWJlLWY1Y2UyNDRjMDE2NyIsImMiOjR9&pageName=ReportSection9014768649565d07e200" frameborder="0" allowFullScreen="true"></iframe>
 </body>
 </html>
