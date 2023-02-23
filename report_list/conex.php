2474e74dd6889a3a1c0e0bec81044f457e12aea4
Brayan Mauricio Jimenez Salamanca <bjimenez@app-peoplemarketing.com>
2023-02-23 09:16:28 -0500

    conex

--- report_list/conex.php
+++ report_list/conex.php
@@ -1,8 +1,8 @@
 <?PHP
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
-$basepaciente = "ipsen";
+$servidor = "app-peoplemarketing.ckkjycussdkq.us-east-1.rds.amazonaws.com";
+$usuario = "apppeopl";
+$password = "ser1_pE0p1E*2018";
+$basepaciente = "apppeopl_ipsen";
 
 $conex = mysqli_connect($servidor, $usuario, $password) or die("No se Puede conectar al Servidor");
 mysqli_select_db($conex, $basepaciente) or die("No se Puede conectar a la base de Datos");
