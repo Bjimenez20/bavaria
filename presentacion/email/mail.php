c892d87d0cd56359a05bf40ccbae90bb79907d31
Brayan Mauricio Jimenez Salamanca <bjimenez@app-peoplemarketing.com>
2023-03-23 17:13:49 -0500

    Update mail.php

--- presentacion/email/mail.php
+++ presentacion/email/mail.php
@@ -18,7 +18,7 @@ $subject = "Reporte Farmacovigilancia - $INICIALES_PACIENTE - $fecha";
 $mail->Body = $body;
 $mail->Subject = $subject;
 $mail->addAddress('bjimenez@app-peoplemarketing.com');
-// $mail->addCC('diego.enrique.orjuela@ipsen.com');
+$mail->addCC('dmendoza@peoplemarketing.com.co');
 // $mail->addBCC('dmendoza@peoplemarketing.com.co');
 $mail->addAttachment('../EVENTO_ADVERSO/' . $ID_EVENTO_ADVERSO . '/Evento_Adverso_' . $ID_PACIENTE . '.pdf');
 $mail->Send() ? "Enviado" : "Problema al enviar";
