f8802acaa5aedc99b4127329f4cbf2147f747b17
Brayan Mauricio Jimenez Salamanca <bjimenez@app-peoplemarketing.com>
2023-04-10 21:08:04 -0500

    emails ea

--- presentacion/email/mail.php
+++ presentacion/email/mail.php
@@ -17,11 +17,10 @@ $subject = "Prueba reporte Farmacovigilancia - PAP$ID_PAP - $fecha";
 
 $mail->Body = $body;
 $mail->Subject = $subject;
-$mail->addAddress('bjimenez@app-peoplemarketing.com');
-// $mail->addAddress('pharmacovigilance.colombia@ipsen.com');
-// $mail->addAddress('diego.enrique.orjuela@ipsen.com');
-// $mail->addCC('dmendoza@peoplemarketing.com.co');
-// $mail->addBCC('bjimenez@app-peoplemarketing.com');
+$mail->addAddress('pharmacovigilance.colombia@ipsen.com');
+$mail->addAddress('diego.enrique.orjuela@ipsen.com');
+$mail->addCC('dmendoza@peoplemarketing.com.co');
+$mail->addBCC('bjimenez@app-peoplemarketing.com');
 $mail->addAttachment('../EVENTO_ADVERSO/' . $ID_EVENTO_ADVERSO . '/Evento_Adverso_' . $ID_PAP . '.pdf');
 $mail->Send() ? "Enviado" : "Problema al enviar";
 $mail->smtpClose();
