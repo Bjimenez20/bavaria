cd28d31822d5debca4c437f3202c48e5df295f4f
Bjimenez20 <112726778+Bjimenez20@users.noreply.github.com>
2023-06-20 16:03:47 -0500

    Update form_restablecer_clave.php

--- presentacion/form_restablecer_clave.php
+++ presentacion/form_restablecer_clave.php
@@ -8,7 +8,9 @@ include('../logica/session.php')
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
    <link rel="stylesheet" href="../presentacion/css/estilos_menu.css" />
    <link type="text/css" rel="stylesheet" href="../presentacion/css/estilo_form_paciente.css" />
-   <title>IPSEN</title>
+   <title>Documento sin título</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <style type="text/css">
    @import url("GothamRnd_book/stylesheet.css");
@@ -17,8 +19,12 @@ include('../logica/session.php')
       text-align: center;
    }
 
-   form {
-      background: url('../presentacion/imagenes/fondo_nueva_cl.png') top left no-repeat;
+   body {
+      background: url('../layouts/img/background.png');
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
    }
 
    .fuente {
@@ -26,16 +32,29 @@ include('../logica/session.php')
    }
 
    .error {
-      font-family: Tahoma, Geneva, sans-serif;
+      font-family: GothamRnd-book;
       color: #C30;
    }
 
-   html {
-      background: url(../presentacion/imagenes/FONDO.png) no-repeat fixed center;
-      -webkit-background-size: cover;
-      -moz-background-size: cover;
-      -o-background-size: cover;
-      background-size: cover;
+   .aviso3 {
+      font-size: 130%;
+      font-weight: bold;
+      color: #11a9e3;
+      text-transform: uppercase;
+      font-family: Tahoma, Geneva, sans-serif;
+      background-color: transparent;
+      text-align: center;
+      padding: 10px;
+   }
+
+   .error {
+      font-size: 130%;
+      font-weight: bold;
+      color: #E30613;
+      text-transform: uppercase;
+      background-color: transparent;
+      text-align: center;
+      padding: 10px;
    }
 </style>
 
@@ -46,102 +65,85 @@ include('../logica/session.php')
    function validar_clave($clave, &$error_clave)
    {
       if (strlen($clave) < 8) {
-         $error_clave = "<span class=error>La clave debe tener al menos 8 caracteres</span>";
+         $error_clave = "La clave debe tener al menos 8 caracteres";
          return false;
       }
       if (strlen($clave) > 16) {
-         $error_clave = "<span class=error>La clave no puede tener más de 16 caracteres</span>";
+         $error_clave = "La clave no puede tener más de 16 caracteres";
          return false;
       }
       if (!preg_match('`[a-z]`', $clave)) {
-         $error_clave = "<span class=error>La clave debe tener al menos una letra minúscula</span>";
+         $error_clave = "La clave debe tener al menos una letra minúscula";
          return false;
       }
       if (!preg_match('`[A-Z]`', $clave)) {
-         $error_clave = "<span class=error>La clave debe tener al menos una letra mayúscula</span>";
+         $error_clave = "La clave debe tener al menos una letra mayúscula";
          return false;
       }
       if (!preg_match('`[0-9]`', $clave)) {
-         $error_clave = "<span class=error>La clave debe tener al menos un caracter numérico</span>";
+         $error_clave = "La clave debe tener al menos un caracter numérico";
          return false;
       }
       $error_clave = "";
       return true;
    }
    ?>
-   <center>
-      <table width="906" border="0" cellspacing="1" cellpadding="2">
-         <tr>
-            <th width="275" height="50" scope="row">&nbsp;</th>
-            <td width="333">&nbsp;</td>
-            <td width="282">&nbsp;</td>
-         </tr>
-         <tr>
-            <th height="268" scope="row">&nbsp;</th>
-            <td>
-               <form id="inicio" action="../presentacion/form_restablecer_clave.php" method="POST" style="width:100%;" target="info" class="letra">
-                  <section style="width:100%; height:100%; padding:0;  text-align:center">
-                     <br />
-                     <br />
-                     <br />
-                     <br />
-                     <span class="fuente">Contrase&ntilde;a actual &nbsp;&nbsp;</span>
-                     <input id="Contrasena_ac" name="Contrasena_ac" type="password" required="required" title="ESCRIBA UNA CONTRASEÑA CORRECTA" />
-                     <br />
-                     <br />
-                     <span class="fuente">Nueva Contrase&ntilde;a &nbsp;&nbsp;</span>
-                     <input id="Contrasena_nu" name="Contrasena_nu" type="password" required="required" title="ESCRIBA UNA CONTRASEÑA CORRECTA" />
-                     <br />
-                     <br />
-                     <span class="fuente">Reperir Contrase&ntilde;a </span>
-                     <input id="Contrasena_va" name="Contrasena_va" type="password" required="required" title="ESCRIBA UNA CONTRASEÑA CORRECTA" />
-                     <br />
-                     <br />
-                     <?php
-                     if (isset($_POST['InicioR'])) {
-                        $CONTRASENA_AC = $_POST['Contrasena_ac'];
-                        $CONTRASENA_NU = $_POST['Contrasena_nu'];
-                        $CONTRASENA_VA = $_POST['Contrasena_va'];
-                        $CONTRASENA_VENCE = date('Y-m-d  H:i:s', strtotime('+1 month'));
-                        $error_encontrado = "";
-                        if (validar_clave($_POST["Contrasena_nu"], $error_encontrado)) {
-                           if ($CONTRASENA_NU == $CONTRASENA_VA) {
-                              echo "<span class=fuente>CONTRASE&Ntilde;A V&Aacute;LIDA</span>";
-                              $sql = mysqli_query($conex, "UPDATE ipsen_usuario SET 
+   <div class="row m-0">
+      <div class="col-md-6 col-lg-5 col-sm-10 col-xs-10 mx-auto m-5">
+         <form id="inicio" action="../presentacion/form_restablecer_clave.php" method="POST" style="width:100%;">
+            <div class="row-reverse my-5">
+               <div class="col d-flex justify-content-center align-items-center mb-3">
+                  <img src="../presentacion/layouts/img/3.png" alt="" class="w-50">
+               </div>
+               <div class="col-10 mx-auto mb-4">
+                  <div class="input-group mb-4">
+                     <input id="Contrasena_ac" name="Contrasena_ac" type="password" required="required" class="form-control" placeholder="Contraseña actual">
+                  </div>
+               </div>
+               <div class="col-10 mx-auto mb-4">
+                  <div class="input-group mb-4">
+                     <input id="Contrasena_nu" name="Contrasena_nu" type="password" required="required" class="form-control" placeholder="Contraseña nueva">
+                  </div>
+               </div>
+               <div class="col-10 mx-auto mb-4">
+                  <div class="input-group mb-4">
+                     <input id="Contrasena_va" name="Contrasena_va" type="password" required="required" class="form-control" placeholder="Confirmar contraseña">
+                  </div>
+               </div>
+            </div>
+            <center>
+               <input id="InicioR" name="InicioR" type="submit" value="INICIAR SESION" class="btn_continuar" />
+            </center>
+            <?php
+            if (isset($_POST['InicioR'])) {
+               $CONTRASENA_AC = $_POST['Contrasena_ac'];
+               $CONTRASENA_NU = $_POST['Contrasena_nu'];
+               $CONTRASENA_VA = $_POST['Contrasena_va'];
+               $CONTRASENA_VENCE = date('Y-m-d  H:i:s', strtotime('+1 month'));
+               $error_encontrado = "";
+               if (validar_clave($_POST["Contrasena_nu"], $error_encontrado)) {
+                  if ($CONTRASENA_NU == $CONTRASENA_VA) {
+                     echo "<span class=fuente>CONTRASE&Ntilde;A V&Aacute;LIDA</span>";
+                     $sql = mysqli_query($conex, "UPDATE ipsen_usuario SET 
                               CONTRASENA = '" . MD5($CONTRASENA_NU) . "',
                               CONTRASENA_FECHA = '" . $CONTRASENA_VENCE . "'
                               WHERE USER='" . $USUARIO . "';");
-                              echo mysqli_error($conex);
-                              header("Location: ../");
-                              session_unset();
-                              session_destroy();
-                              exit();
-                           } else {
-                              echo "<span class=error>Las contrase&ntilde;as no coinciden</span>";
-                           }
-                        } else {
-                           echo "<span class=error>CONTRASE&Ntilde;A NO V&Aacute;LIDA </span>" . $error_encontrado;
-                        }
-                     }
-                     ?>
-                     <br />
-                     <center>
-                        <input id="InicioR" name="InicioR" type="submit" value="INICIAR SESION" class="btn_continuar" />
-                        <br />
-                        <br />
-                     </center>
-                  </section>
-               </form>
-            </td>
-            <td>&nbsp;</td>
-         </tr>
-         <tr>
-            <th scope="row">&nbsp;</th>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-         </tr>
-      </table>
-   </center>
+                     echo mysqli_error($conex);
+                     header("Location: ../");
+                     session_unset();
+                     session_destroy();
+                     exit();
+                  } else {
+                     echo "<span class=error>Las contrase&ntilde;as no coinciden</span>";
+                  }
+               } else {
+                  echo "<span class=error>CONTRASE&Ntilde;A NO V&Aacute;LIDA </span>" . $error_encontrado;
+               }
+            }
+            ?>
+         </form>
+      </div>
+   </div>
 </body>
 
 </html>
