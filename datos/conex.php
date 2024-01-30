1f5dba0faf0fb061b6fa3eec103ee12730d99ed6
Bjimenez20 <112726778+Bjimenez20@users.noreply.github.com>
2024-01-30 12:59:25 +0000

    fest: conex

--- datos/conex.php
+++ datos/conex.php
@@ -4,15 +4,15 @@
 // $password = "ipsen";
 // $basepaciente = "ipsen_test";
 
-$servidor = "host.docker.internal";
-$usuario = "root";
-$password = "root";
-$basepaciente = "apppeopl_ipsen";
+// $servidor = "host.docker.internal";
+// $usuario = "root";
+// $password = "root";
+// $basepaciente = "apppeopl_ipsen";
 
-// $servidor = "app-peoplemarketing.ckkjycussdkq.us-east-1.rds.amazonaws.com";
-// $usuario = "apppeopl_labs";
-// $password = "u@U*5B7tOv3i";
-// $basepaciente = "apppeopl_ipsen_test";
+$servidor = "app-peoplemarketing.ckkjycussdkq.us-east-1.rds.amazonaws.com";
+$usuario = "apppeopl_labs";
+$password = "u@U*5B7tOv3i";
+$basepaciente = "apppeopl_ipsen_test";
 
 $conex = mysqli_connect($servidor, $usuario, $password) or die("No se Puede conectar al Servidor");
 $conex->set_charset('utf8mb4');
