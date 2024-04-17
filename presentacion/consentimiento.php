5554c421318580ec221d8b7bc573cd4397fde3a3
Bjimenez20 <112726778+Bjimenez20@users.noreply.github.com>
2024-04-17 12:43:36 +0000

    chore: modificaciones link ci

--- presentacion/consentimiento.php
+++ presentacion/consentimiento.php
@@ -4,7 +4,7 @@ $ID_GESTION_ULT = $_POST['ID_GESTION_ULT'];
 // $PAP = base64_encode($ID_PACIENTE);
 // $GES = base64_encode($ID_GESTION_ULT);
 $time = strtotime("now +168 hours");
-$dominio = 'http://localhost:8000';
+$dominio = 'http://ec2-34-233-161-124.compute-1.amazonaws.com:8007';
 $url = "{$dominio}/presentacion/plantilla_ci.php?pap={$ID_PACIENTE}&ges={$ID_GESTION_ULT}&expires={$time}";
 $key = "base64:yv051saGBdRpua6fS3ec5gR8jeymLoIfnejGQSzj70g=";
 $signature = hash_hmac("sha256", $url, $key);
@@ -13,4 +13,3 @@ $signature = hash_hmac("sha256", $url, $key);
 $url_with_signature = $url . "&signature=" . $signature;
 
 echo '<input type="text" value=' . $url_with_signature . ' readonly id="url_consentimiento" style="display: none; width:100%;">';
-?>
